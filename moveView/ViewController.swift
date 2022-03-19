8a9888121cac252ebfd6fd06594e5440dc4961e3
JunYoung Lee <52552781+junlight94@users.noreply.github.com>
2022-03-19 22:43:04 +0900

    fir

--- moveView/ViewController.swift
+++ moveView/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var moveView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(drag))
+        moveView.addGestureRecognizer(gesture)
+    }
+    
+    @objc func drag(_ sender: UIPanGestureRecognizer) {
+        
+
+        //손가락의 위치를 point에 저장
+        let point = sender.location(in: view)
+
+        //손가락이 클릭하고 있는 view를 draggedView에 저장
+        let draggedView = sender.view!
+
+        //draggedView의 중심을 손가락 위치인 point에 따라 움직인다
+        draggedView.center = point
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(moveView.frame.origin.x)
+        print(moveView.frame.origin.y)
+        
     }
 
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
 
 }
 
